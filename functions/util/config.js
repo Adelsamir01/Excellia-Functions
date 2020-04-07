2bd3dde058b86ffcab7feab6d06e69d17cc83c25
Adel Samir ElZemity <43517848+Adelsamir01@users.noreply.github.com>
2020-04-07 04:09:20 +0200

    1

---
+++ functions/util/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: 'AIzaSyBGE61bBXSyRfOccvujvR8VjWfB1BjW-Og',
+  authDomain: 'excellia-4c9b1.firebaseapp.com',
+  databaseURL: 'https://excellia-4c9b1.firebaseio.com',
+  projectId: 'excellia-4c9b1',
+  storageBucket: 'excellia-4c9b1.appspot.com',
+  messagingSenderId: '354040522108'
+};
